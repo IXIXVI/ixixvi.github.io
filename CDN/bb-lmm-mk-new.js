c0f921f7c47295e39838d627d8df9655c65a0c36
Yining Zhou <35400164+IXIXVI@users.noreply.github.com>
2024-05-08 22:17:38 +0800

    Update bb-lmm-mk-new.js

--- CDN/bb-lmm-mk-new.js
+++ CDN/bb-lmm-mk-new.js
@@ -2,7 +2,7 @@
 Last Modified time : 20230926 11:00 by https://immmmm.com
 */
 
-function fn(){
+function fn(){  
   var bbMemo = {
     memos: 'https://demo.usememos.com/',
     limit: '10',
@@ -262,7 +262,13 @@ function fn(){
     const TAG_REG = /#([^#\s!.,;:?"'()]+)(?= )/g ///#([^/\s#]+?) /g
     , IMG_REG = /\!\[(.*?)\]\((.*?)\)/g
     , LINK_REG = /\[(.*?)\]\((.*?)\)/g
-    , DEODB_LINK_REG = /(https:\/\/(www|movie|book)\.douban\.com\/(game|subject)\/[0-9]+\/).*?/g;
+    , DEODB_LINK_REG = /(https:\/\/(www|movie|book)\.douban\.com\/(game|subject)\/[0-9]+\/).*?/g
+    , BILIBILI_REG = /<a.*?href="https:\/\/www\.bilibili\.com\/video\/((av[\d]{1,10})|(BV([\w]{10})))\/?".*?>.*<\/a>/g
+    , NETEASE_MUSIC_REG = /<a.*?href="https:\/\/music\.163\.com\/.*id=([0-9]+)".*?>.*<\/a>/g
+    , QQMUSIC_REG = /<a.*?href="https\:\/\/y\.qq\.com\/.*(\/[0-9a-zA-Z]+)(\.html)?".*?>.*?<\/a>/g
+    , QQVIDEO_REG = /<a.*?href="https:\/\/v\.qq\.com\/.*\/([a-z|A-Z|0-9]+)\.html".*?>.*<\/a>/g
+    , YOUKU_REG = /<a.*?href="https:\/\/v\.youku\.com\/.*\/id_([a-z|A-Z|0-9|==]+)\.html".*?>.*<\/a>/g
+    , YOUTUBE_REG = /<a.*?href="https:\/\/www\.youtube\.com\/watch\?v\=([a-z|A-Z|0-9]{11})\".*?>.*<\/a>/g;
     marked.setOptions({
       breaks: false,
       smartypants: false,
@@ -301,7 +307,13 @@ function fn(){
           bbContREG =  bbContTag + bbContREG.trim()
         }
               
-        
+        bbContREG = marked.parse(bbContREG)
+          .replace(BILIBILI_REG, "<div class='video-wrapper'><iframe src='//www.bilibili.com/blackboard/html5mobileplayer.html?bvid=$1&as_wide=1&high_quality=1&danmaku=0' scrolling='no' border='0' frameborder='no' framespacing='0' allowfullscreen='true'></iframe></div>")
+          .replace(NETEASE_MUSIC_REG, "<meting-js auto='https://music.163.com/#/song?id=$1'></meting-js>")
+          .replace(QQMUSIC_REG, "<meting-js auto='https://y.qq.com/n/yqq/song$1.html'></meting-js>")
+          .replace(QQVIDEO_REG, "<div class='video-wrapper'><iframe src='//v.qq.com/iframe/player.html?vid=$1' allowFullScreen='true' frameborder='no'></iframe></div>")
+          .replace(YOUKU_REG, "<div class='video-wrapper'><iframe src='https://player.youku.com/embed/$1' frameborder=0 'allowfullscreen'></iframe></div>")
+          .replace(YOUTUBE_REG, "<div class='video-wrapper'><iframe src='https://www.youtube.com/embed/$1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen title='YouTube Video'></iframe></div>")
   
         //解析 content 内 md 格式图片
         let IMG_ARR = data[i].content.match(IMG_REG) || '',IMG_ARR_Grid='';
@@ -575,6 +587,7 @@ function fn(){
       ArtalkDom_ID.remove();
     }
   }
+
 }
 
 fn();
